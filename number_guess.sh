6adddc3fa1a0e65107f8e03b87903c89d5fd77f1
Bhavy Upreti <102848790+bhavyup@users.noreply.github.com>
2026-02-07 05:40:40 -0500

    Initial commit

---
+++ number_guess.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+# Generate random number between 1 and 1000
+SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+# Prompt for username
+echo "Enter your username:"
+read USERNAME
+
+# Check if user exists
+USER_DATA=$($PSQL "SELECT games_played, best_game FROM users WHERE username='$USERNAME'")
+
+if [[ -z $USER_DATA ]]
+then
+  # New user
+  echo "Welcome, $USERNAME! It looks like this is your first time here."
+  INSERT_USER_RESULT=$($PSQL "INSERT INTO users(username) VALUES('$USERNAME')")
+else
+  # Returning user
+  IFS="|" read GAMES_PLAYED BEST_GAME <<< "$USER_DATA"
+  echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+# Start guessing game
+echo "Guess the secret number between 1 and 1000:"
+NUMBER_OF_GUESSES=0
+
+while read GUESS
+do
+  # Check if input is an integer
+  if [[ ! $GUESS =~ ^[0-9]+$ ]]
+  then
+    echo "That is not an integer, guess again:"
+  else
+    ((NUMBER_OF_GUESSES++))
+    
+    if [[ $GUESS -eq $SECRET_NUMBER ]]
+    then
+      echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $SECRET_NUMBER. Nice job!"
+      
+      # Update database
+      CURRENT_BEST=$($PSQL "SELECT best_game FROM users WHERE username='$USERNAME'")
+      
+      if [[ -z $CURRENT_BEST || $NUMBER_OF_GUESSES -lt $CURRENT_BEST ]]
+      then
+        UPDATE_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1, best_game = $NUMBER_OF_GUESSES WHERE username='$USERNAME'")
+      else
+        UPDATE_RESULT=$($PSQL "UPDATE users SET games_played = games_played + 1 WHERE username='$USERNAME'")
+      fi
+      
+      break
+    elif [[ $GUESS -gt $SECRET_NUMBER ]]
+    then
+      echo "It's lower than that, guess again:"
+    else
+      echo "It's higher than that, guess again:"
+    fi
+  fi
+done
